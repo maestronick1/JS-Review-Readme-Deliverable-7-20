bc6eae7392cd5622932447ead0147d6bbe592fe5
Nicholas Phillips <nicholasphillips@Nicholass-MacBook-Air.local>
2020-07-20 14:13:33 -0700

    add own object

---
+++ app.js
@@ -0,0 +1,43 @@
+let name = "Nick Phillips" ;
+let age = 32;
+let isCool = true;
+const friends = [ "Matt", "John", "Andrew"]
+
+
+const tesla ={
+    industry: "cars",
+    founder:    "Musk",
+    year:        "2003",
+    memebers:   ["Straubel", "eberhart"],
+    stockTicker: "TSLA",
+    vehicles: {
+        vehicleOne: "Model S",
+        vehicleTwo: "Model X",
+        vehicleThree: "Model J",
+
+
+    },
+    print: function (){
+        console.log("The CEO of TEsla is ' + this.ceo");
+    }
+}
+tesla.stockPrice = 1643.00;
+console.log(tesla)
+console.log(name);
+console.log(age);
+console.log(isCool);
+console.log(friends);
+console.log(tesla.vehicles.vehicleThree)
+console.log(tesla.vehicles.vehicleTwo)
+
+tesla.print();
+
+
+// make your own object
+let composer = "Bethoven";
+let age = 56;
+let beautifulMusic = true;
+let instrumentsPlayed= ["piano", "violin",]
+
+
+
